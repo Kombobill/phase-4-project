177e970c3f174c5ac0230091f6b2d7c76cd78483
Osorabrian <osorabrian@gmail.com>
2023-04-01 11:44:25 +0300

    create appointments page

--- client/src/components/viewappointment.js
+++ client/src/components/viewappointment.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import '../styling/viewappointments.css'
 
 
 export default function ViewAppointment(){
+
     return(
-        <div>
-            
+        <div id='all-appointments-page'> 
+
+            <div id='welcome-message'>
+                <h1>Hi, name</h1>
+                <h3>Here is a list of all your appointments.</h3>
+            </div>
+
+            <table className='table'  id='appointments-table'>
+
+                <thead className='bg-primary' >
+                    <tr className='text-light'>
+                        <td>No.</td>
+                        <td>Doctor ID.</td>
+                        <td>Doctor Name</td>
+                        <td>Speciality</td>
+                        <td>Date</td>
+                        <td>Duration</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>3</td>
+                        <td>Patrick</td>
+                        <td>Paeditrician</td>
+                        <td>26/5/2023</td>
+                        <td>1 hour</td>
+                        <td className='row'>
+                            <div className='col-6'>
+                                <button className='btn btn-info'>Update</button>
+                            </div>
+
+                            <div className='col-6'>
+                                <button className='btn btn-danger'>Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
+
         </div>
     )
 }
