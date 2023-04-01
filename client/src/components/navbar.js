966bd787957f595f8fce675d9c4f5f5313f49eb3
DOROTHY2322 <kimelidorothy@gmail.com>
2023-04-01 19:28:15 +0300

    navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+
+    const navigate = useNavigate()
+
     function handleLogOut() {
         fetch('/logout', {
             method: 'DELETE',
         })
             .then(response => {
                 if (response.ok) {
-                    // reload the current page after logout to update the state of the app
-                    window.location.reload()
+                    navigate('/')
                 }
             })
     }
@@ -20,16 +22,15 @@ export default function NavBar() {
                 <img src='https://raw.githubusercontent.com/Kombobill/phase-4-project/master/client/public/central_hospital_logo.jpg' alt="Central Hospital" style={{ width: '70px', height: '70px' }} />
             </Link>
             <ul className='nav justify-content-center'>
-                
                 <li className='nav-item'>
                     <Link className='nav-link' to='/appointments'>Appointments</Link>
                 </li>
-              
             </ul>
             <div className='nav justify-content-end'>
-                <li className='nav-item'>
+            <li className='nav-item'>
                     <Link className='nav-link' to='/'>
-                        <i className="bi bi-house-door me-1"></i>  
+                        <i className="bi bi-house-door me-1"></i>
+                        
                     </Link>
                 </li>
                 <li className='nav-item'>
