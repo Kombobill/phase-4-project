ba953224b26d05f00397a8b81e149e0326d2fceb
Bob Oyier <boboyier92@gmail.com>
2023-04-02 00:28:37 +0300

    Co-authored-by: Faith chemutai <Faith-chemutai@users.noreply.github.com> Co-authored-by: brian_osora <Osorabrian@users.noreply.github.com> Co-authored-by: Kombobill <Kombobill@users.noreply.github.com> Co-authored-by: DOROTHY2322 <DOROTHY2322@users.noreply.github.com>

--- client/src/components/ForgetPasswordPage.js
+++ client/src/components/ForgetPasswordPage.js
@@ -1,70 +1,81 @@
-import React, {useState} from 'react'
-import '../styling/forgotpassword.css'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import '../styling/forgotpassword.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function ForgetPasswordPage() {
+	const [userName, setUserame] = useState('');
+	const [password, setPassword] = useState('');
+	const [errors, setErrors] = useState([]);
 
-    const[userName, setUserame] = useState('')
-    const[password, setPassword] = useState('')
-    const[errors, setErrors] = useState([])
+	const navigate = useNavigate();
 
-    const navigate = useNavigate()
+	const resetData = { password };
 
-    const resetData = {password}
+	function handleResetPassword(e) {
+		e.preventDefault();
+		fetch(`/reset-password/${userName}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(resetData),
+		}).then((response) => {
+			if (response.ok) {
+				response.json().then((data) => console.log(data));
+				navigate('/');
+			} else {
+				response.json().then((errorData) => setErrors(errorData.error));
+			}
+		});
+	}
 
-    function handleResetPassword(e){
-        e.preventDefault()
-        fetch(`/reset-password/${userName}`,{
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(resetData)
-        })
-        .then(response => {
-            if(response.ok){
-                response.json().then(data => console.log(data))
-                navigate('/')
-            }else{
-                response.json().then(errorData => setErrors(errorData.error))
-            }
-        })
-    }
+	return (
+		<div id="forgot-password-page">
+			<form id="reset-password-form" onSubmit={handleResetPassword}>
+				<h2>Reset your password</h2>
 
-    return (
-        <div id="forgot-password-page">
+				{errors.length > 0 && (
+					<ul style={{ color: 'red' }}>
+						<li>{errors}</li>
+					</ul>
+				)}
 
-            <form id='reset-password-form' onSubmit={handleResetPassword}>
+				<div className="row mb-3">
+					<label className="form-label">Username</label>
+					<br />
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter username"
+						required
+						onChange={(e) => setUserame(e.target.value)}
+					/>
+				</div>
 
-                <h2>Reset your password</h2>
+				<div className="row mb-3">
+					<label className="form-label">Password</label>
+					<br />
+					<input
+						className="form-control"
+						type="password"
+						placeholder="Enter Password"
+						required
+						autoComplete="true"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
 
-                {
-                    errors.length > 0 && (
-                       <ul style={{color: 'red'}}>
-                            <li>{errors}</li>
-                        </ul> 
-                    )
-                }
+				<p className="form-text">
+					<Link to="/">Back to Login</Link>.
+				</p>
 
-                <div className='row mb-3'>
-                    <label className="form-label">Username</label><br/>
-                    <input className="form-control" type="text" placeholder="Enter username" required onChange={e => setUserame(e.target.value)}/>
-                </div>
-
-                <div className='row mb-3'>
-                    <label className="form-label">Password</label><br/>
-                    <input className="form-control" type="password" placeholder="Enter Password" required autoComplete='true' onChange={e => setPassword(e.target.value)}/>
-                </div>
-
-                <p className='form-text'><Link to="/">Back to Login</Link>.</p>
-
-                <div style={{textAlign: 'center'}}>
-                    <button className="btn btn-primary" type="submit">Reset password</button>
-                </div>
-                
-            </form>
-           
-        </div>
-    )
+				<div style={{ textAlign: 'center' }}>
+					<button className="btn btn-primary" type="submit">
+						Reset password
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,50 +2,57 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate()
+	function handleLogOut() {
+		fetch('/logout', {
+			method: 'DELETE',
+		}).then((response) => {
+			if (response.ok) {
+				navigate('/');
+			}
+		});
+	}
 
-    function handleLogOut() {
-        fetch('/logout', {
-            method: 'DELETE',
-        })
-            .then(response => {
-                if (response.ok) {
-                    navigate('/')
-                }
-            })
-    }
-
-    return (
-        <nav className="navbar">
-            <Link className="navbar-brand" to='/'>
-                <img src='https://raw.githubusercontent.com/Kombobill/phase-4-project/master/client/public/central_hospital_logo.jpg' alt="Central Hospital" style={{ width: '70px', height: '70px' }} />
-            </Link>
-            <ul className='nav justify-content-center'>
-                <li className='nav-item'>
-                    <Link className='nav-link' to='/appointments'>Appointments</Link>
-                </li>
-            </ul>
-            <div className='nav justify-content-end'>
-            <li className='nav-item'>
-                    <Link className='nav-link' to='/'>
-                        <i className="bi bi-house-door me-1"></i>
-                        
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link className='nav-link' to='/'>
-                        <i className="bi bi-house-door me-1"></i>
-                        Log in
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link className='nav-link' to='/register'>Sign Up</Link>
-                </li>
-                <li className='nav-item'>
-                    <button className='nav-link btn btn-link' onClick={handleLogOut}>Log Out</button>
-                </li>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className="navbar">
+			<Link className="navbar-brand" to="/">
+				<img
+					src="https://raw.githubusercontent.com/Kombobill/phase-4-project/master/client/public/central_hospital_logo.jpg"
+					alt="Central Hospital"
+					style={{ width: '70px', height: '70px' }}
+				/>
+			</Link>
+			<ul className="nav justify-content-center">
+				<li className="nav-item">
+					<Link className="nav-link" to="/appointments">
+						Appointments
+					</Link>
+				</li>
+			</ul>
+			<div className="nav justify-content-end">
+				<li className="nav-item">
+					<Link className="nav-link" to="/">
+						<i className="bi bi-house-door me-1"></i>
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/">
+						<i className="bi bi-house-door me-1"></i>
+						Log in
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/register">
+						Sign Up
+					</Link>
+				</li>
+				<li className="nav-item">
+					<button className="nav-link btn btn-link" onClick={handleLogOut}>
+						Log Out
+					</button>
+				</li>
+			</div>
+		</nav>
+	);
 }
