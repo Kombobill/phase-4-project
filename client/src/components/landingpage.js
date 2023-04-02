17e8739352e716e5bdd3f8639a493886c2aaf5da
DOROTHY2322 <kimelidorothy@gmail.com>
2023-04-02 17:52:22 +0300

    landing page

--- client/src/components/landingpage.js
+++ client/src/components/landingpage.js
@@ -11,20 +11,21 @@ export default function LandingPage(){
         <div id='landng-page'>
             <img src={appointmentImage} alt="background_image" id='background-image'/>
             <div style={{marginLeft: '30px', marginTop: '8px'}}>
-                <h1 className='text-primary' style={{fontSize: '90px'}}>BOOK DOCTOR</h1>
-                <h1 className='text-primary' style={{fontSize: '80px'}}>APPOINTMENT</h1>
-                <h1 className='text-primary' style={{fontSize: '60px'}}>ONLINE</h1>
+                <h1 className='text-dark' style={{fontSize: '90px'}}>BOOK YOUR</h1>
+                <h1 className='text-dark' style={{fontSize: '80px'}}>APPOINTMENT</h1>
+                <h1 className='text-dark' style={{fontSize: '60px'}}>ONLINE</h1>
             </div>
             {
                 !auth.user && (
-                    <div className='row mt-5'>
-                        <div className='col-4'>
-                            <Link to='/register' className='btn btn-light text-primary border-primary' style={{marginLeft: '50px'}}>Register</Link>
-                        </div>
-                        <div className='col-4'>
-                            <Link to='/login' className='btn btn-light text-primary border-primary' style={{marginLeft: '-190px'}}>Log In</Link>
-                        </div>
+                    <div className='row mt-2 text-center'>
+                    <div className='col-4'>
+                      <Link to='/register' className='btn btn-light text-primary border-primary font-weight-bold'>Register</Link>
                     </div>
+                    <div className='col-4'>
+                      <Link to='/login' className='btn btn-light text-primary border-primary font-weight-bold'>Log In</Link>
+                    </div>
+                  </div>
+                  
                 )
             }
             <div className='about-us-section'>
