9087f51fd8f9106bd2c4519aaaa6d64d8d5abe00
Osorabrian <osorabrian@gmail.com>
2023-04-03 06:30:56 +0300

    edit origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://6429c255a714793536aef067--quikhealth.netlify.app/'
+      origins '*'
       resource '*', 
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head, :show_appointments, :create_appointment],
